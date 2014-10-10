2756b888470fd24a9b78b7dfb1471b42d4e81647
sw4 <samuel.wheeler@gmail.com>
2014-10-10 16:32:14 +0100

    Update pk-scroll.js
    Updated keyboard binding routine

--- pk-scroll.js
+++ pk-scroll.js
@@ -130,8 +130,6 @@ var pk = pk || {};
             }
         }, 500);
 
-
-
         // DRAG HANDLERS
 
         if(allowY){
@@ -263,51 +261,131 @@ var pk = pk || {};
         // KEYBOARD HANDLERS    
         container.setAttribute("tabindex", 0);
         pk.bindEvent('keydown', container, function (e) {
-
-            switch (e.keyCode) {
-                case 38:
-                    //up cursor
-                    if (!allowY) return;
-                    container.scrollTop -= containerH * 0.1;
-                    break;
-                case 40:
-                    //down cursor
-                    if (!allowY) return;
-                    container.scrollTop += containerH * 0.1;
-                    break;
-                case 37:
-                    //left cursor
-                    if (!allowX) return;
-                    container.scrollLeft -= containerW * 0.1;
-                    break;
-                case 39:
-                    //right cursor
-                    if (!allowX) return;
-                    container.scrollLeft += containerW * 0.1;
-                    break;
-                case 33:
-                    //page up
-                    if (!allowY) return;
-                    container.scrollTop -= containerH;
-                    break;
-                case 34:
-                    //page down
-                    if (!allowY) return;
-                    container.scrollTop += containerH;
-                    break;
-                case 36:
-                    //home
-                    if (!allowY) return;
-                    container.scrollTop = 0;
-                    break;
-                case 35:
-                    //end
-                    if (!allowY) return;
-                    container.scrollTop = contentH;
-                    break;
+            if(allowY){                
+                switch (e.keyCode) {
+                    case 38: //up cursor
+                        container.scrollTop -= containerH * 0.1;
+                        break;
+                    case 40: //down cursor
+                    case 32: //spacebar
+                        container.scrollTop += containerH * 0.1;
+                        break;
+                    case 33: //page up
+                        container.scrollTop -= containerH;
+                        break;
+                    case 34: //page down
+                        container.scrollTop += containerH;
+                        break;
+                    case 36: //home
+                        container.scrollTop = 0;
+                        break;
+                    case 35: //end
+                        container.scrollTop = contentH;
+                        break;
+                }                
+            }
+            if(allowX){                
+                switch (e.keyCode) {
+                    case 37: //left cursor
+                        container.scrollLeft -= containerW * 0.1;
+                        break;
+                    case 39: //right cursor
+                        container.scrollLeft += containerW * 0.1;
+                        break;
+                }                
             }
             pk.preventBubble(e);
         });
     };
     return pk;
+})(pk);
+
+var pk = pk || {};
+(function (pk) {
+    pk.draggable = function (opt) {
+        var drag = false;
+        var el = opt.element;
+        var handle = opt.handle || opt.element;
+        var container = {
+            element: opt.container && opt.container.element ? opt.container.element : document.body,
+            style: opt.container && opt.container.style ? opt.container.style : 'restrict'
+        };
+
+        pk.addClass(handle, 'pk-drag-draggable');
+        var fn = opt.listeners;
+        var m = opt.move;
+        if (m && typeof m !== 'object') {
+            m= {
+                x: true,
+                y: true
+            };
+        }
+        var dragging = false;
+        var dragStart = {}, dragEnd = {};
+        var startOffset;
+        var containerD = {};
+        var elD = {};
+        pk.bindEvent("mousedown", handle, function (e) {
+            dragging = true;
+            dragStart = e.dragStart = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            startOffset = {
+                x: e.clientX - el.getBoundingClientRect().left,
+                y: e.clientY - el.getBoundingClientRect().top
+            };
+            pk.addClass(handle, 'pk-drag-dragging');
+            pk.addClass(document.body, 'pk-noselect');
+            document.onselectstart = function () {
+                return false;
+            };
+            containerD = pk.offset(container.element);
+            elD = pk.offset(el);
+            if (fn && fn.dragstart) fn.dragstart(el, e);
+        });
+        pk.bindEvent("mouseup", window, function (e) {
+            if (!dragging) return;
+            dragging = false;
+            e.dragStart = dragStart;
+            e.dragEnd = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            pk.removeClass(handle, 'pk-drag-dragging');
+            pk.removeClass(document.body, 'pk-noselect');
+            document.onselectstart = function () {
+                return true;
+            };
+            if (m && container.style == "snap") {
+                contain();
+            }
+            if (fn && fn.dragend) fn.dragend(el, e);
+        });
+
+        function contain() {
+            if (m.x && el.offsetLeft < 0) {
+                el.style.left = 0 + 'px';
+            } else if (m.x && el.offsetLeft > container.element.offsetWidth - el.offsetWidth) {
+                el.style.left = container.element.offsetWidth - el.offsetWidth + 'px';
+            }
+            if (m.y && el.offsetTop < 0) {
+                el.style.top = 0 + 'px';
+            } else if (m.y && el.offsetTop > container.element.offsetHeight - el.offsetHeight) {
+                el.style.top = container.element.offsetHeight - el.offsetHeight + 'px';
+            }
+        }
+        pk.bindEvent("mousemove", window, function (e) {
+            if (!dragging) return;
+            e.dragStart = dragStart;
+            e.dragEnd = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            if (m.x) el.style.left = el.offsetLeft + (e.dragEnd.x - el.getBoundingClientRect().left) - startOffset.x + 'px';
+            if (m.y) el.style.top = el.offsetTop + (e.dragEnd.y - el.getBoundingClientRect().top) - startOffset.y + 'px';
+        //    if (container.style == "restrict") contain();
+            if (fn && fn.dragging) fn.dragging(el, e);
+        });
+    };
 })(pk);
