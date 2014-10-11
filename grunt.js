2c62aebec8d213c2c87e3e39d32d1ebf065e36b0
sw4 <samuel.wheeler@gmail.com>
2014-10-11 11:26:28 +0100

    Update grunt.js

--- grunt.js
+++ grunt.js
@@ -15,14 +15,14 @@ module.exports = function(grunt) {
     },
     concat: {
       dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
+        src: ['<banner:meta.banner>', '<file_strip_banner:<%= pkg.name %>.js>'],
+        dest: '<%= pkg.name %>.js'
       }
     },
     min: {
       dist: {
         src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>.min.js'
+        dest: '<%= pkg.name %>.min.js'
       }
     },
     lint: {
