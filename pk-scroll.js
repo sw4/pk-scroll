3739660989d4cae6564cbac5c38aed531f8b1d23
sw4 <samuel.wheeler@gmail.com>
2014-10-13 08:33:06 +0100

    Update pk-scroll.js

--- pk-scroll.js
+++ pk-scroll.js
@@ -1,6 +1,7 @@
 var pk = pk || {};
 (function (pk) {
     // HELPERS FOR jQUERY+ANGULAR
+    var jQuery=jQuery || null;
     if (jQuery && typeof jQuery === 'object') {
         // jquery available
         jQuery.fn.extend({
@@ -12,6 +13,7 @@ var pk = pk || {};
             }
         });
     }
+    var angular=angular || null;
     if (angular && typeof angular === 'object') {
         // angular available
         (
